42337ea308d5f11a0103dfba8f26eae6aa66cf93
MioraMichellee <mioramichelle25@gmail.com>
2024-10-24 10:20:28 +0300

    Tensor dimension

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const a = tf.tensor([[1, 2], [3, 4]]);
-console.log('shape:', a.shape);
-a.print();
+const array = [];
+const shape = [2,3];
+
+for (let i=0; i<6;i++){
+    array[i]= Math.random();
+}
+
+const data = tf.tensor2d(array, shape, 'int32' );
+
+data.print();
