ca0c08b559314642af7a253c392b666a63408a66
MioraMichellee <mioramichelle25@gmail.com>
2024-10-26 16:24:36 -0300

    Tensor plus complexe

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const a = tf.tensor([[1, 2], [3, 4]]);
-console.log('shape:', a.shape);
-a.print();
+const values = [];
+const shape = [2,5,3];
+
+for(let i=0; i<30; i++){
+    values[i]= Math.random();
+}
+
+const data = tf.tensor3d(values, shape,'int32')
+data.print();
+data.dataSync();
+
+
+const test = tf.tensor([[[1], [2]], [[3], [4]]]);
